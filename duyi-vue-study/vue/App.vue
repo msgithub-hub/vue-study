56bda11e65150542000c7384867037bafe15b988
shanshun <shanshun@raybattle.com>
2019-09-17 17:56:12 +0800

    9.17

---
+++ duyi-vue-study/vue/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <!--<div>我爱你！{{ name }}</div>-->
+    <Progress :percentage="30"/>
+</template>
+
+<script>
+    import Progress from "./Progress"
+
+    export default {
+        data() {
+            return {
+                name: "黄金福将",
+            }
+        },
+        components: {
+            Progress
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ duyi-vue-study/vue/Progress.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="progress" style="display: flex;">
+        <div class="progress-bar">
+            <div class="progress-bar-outer" :style="{height:strokeWith +'px'}">
+                <div class="progress-bar-inner" :style="{width:percentage +'%',backgroundColor:stroke}"></div>
+            </div>
+        </div>
+        <div class="progress-text" :style="{fontSize:progressTextFontSize+'px'}">100%</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: '哈哈哈'
+            }
+        },
+        props: {
+            strokeWith: {
+                type: Number,
+                default: 6,
+            },
+            percentage: {
+                type: Number,
+                required: true,
+                default: 0,
+                validator(value) {
+                    return value >= 0 && value <= 100
+                }
+            },
+            stroke() {
+                let color;
+                switch (this.status) {
+                    case 'success':
+                        color = '#13ce66';
+                        break;
+                    case 'exeption':
+                        color = '#ff4949';
+                        break;
+                    default:
+                        color = '#20a0ff';
+                        // break;
+                }
+                return color;
+            },
+        },
+        computed: {
+            progressTextFontSize() {
+                return 12 + this.strokeWith * 0.4
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .progress-bar {
+        width: 90%;
+        /*line-height: 2rem;*/
+        /*margin-left: 5%;*/
+        /*background-color: yellow;*/
+    }
+
+    .progress-bar-outer {
+        background-color: #cccccc;
+        border-radius: 6px;
+
+    }
+
+    .progress-bar-inner {
+        width: 50%;
+        height: 100%;
+        border-radius: 6px;
+        background-color: red;
+        transition: width 0.5s ease;
+    }
+
+    .progress-text {
+        margin-top: -8px;
+        margin-left: 2px;
+    }
+</style>
